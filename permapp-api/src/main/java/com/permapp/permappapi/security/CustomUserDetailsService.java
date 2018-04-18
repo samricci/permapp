6bf86ab1eac108e8dd2dd857f51075c34c5016ef
samricci <samantha_oricci@hotmail.com>
2018-04-18 17:55:38 -0300

    Adiciona Bcript encoder para maior segurança das passwords.

--- permapp-api/src/main/java/com/permapp/permappapi/security/CustomUserDetailsService.java
+++ permapp-api/src/main/java/com/permapp/permappapi/security/CustomUserDetailsService.java
@@ -1,23 +1,25 @@
 package com.permapp.permappapi.security;
 
+import static com.permapp.permappapi.security.SecurityConstants.BYCRIPT;
+
 import org.springframework.security.core.authority.AuthorityUtils;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
-import org.springframework.security.crypto.factory.PasswordEncoderFactories;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Component;
 
 @Component
 public class CustomUserDetailsService implements UserDetailsService {
 
-  private PasswordEncoder encoder = PasswordEncoderFactories.createDelegatingPasswordEncoder();
+  private PasswordEncoder encoder = new BCryptPasswordEncoder(16);
 
   @Override
   public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
     ApplicationUser applicationUser = loadApplicationUserByUsername(username);
-    return new User(applicationUser.getUsername(), encoder.encode(applicationUser.getPassword()),
+    return new User(applicationUser.getUsername(),  BYCRIPT + encoder.encode(applicationUser.getPassword()),
         AuthorityUtils.createAuthorityList("ROLE_USER"));
   }
 
--- permapp-api/src/main/java/com/permapp/permappapi/security/SecurityConstants.java
+++ permapp-api/src/main/java/com/permapp/permappapi/security/SecurityConstants.java
@@ -5,4 +5,11 @@ public class SecurityConstants {
   public static final String TOKEN_PREFIX  = "Bearer";
   public static final String HEADER_STRING = "Authorization";
   public static final long EXPIRATION_TIME = 864_000_000L; //1 dia
+
+  public static final String BYCRIPT       = "{bcrypt}";
+
+  //roles
+  public static final String ADMIN         = "ADMIN";
+  public static final String DEFAULT_USER  = "DEFAULT_USER";
+  public static final String SPECIAL_USER  = "SPECIAL_USER";
 }
